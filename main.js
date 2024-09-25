c91094ebee2e4330a66c40fb7097ec45960da7f6
rafaela <drafaela926@gmail.com>
2024-09-25 11:43:32 -0300

    y

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ function soma(num1,num2){
 
 }
 console.log(soma (3,4));
+<<<<<<< HEAD
 function multiplicacion(num2,num3){
- return num2*num3
+   return num2*num3;
+=======
+function multiplicacion(num2,num3){
+
+>>>>>>> 604c442d616a0f274b69a07c53f73a8f9dc55797
 }
