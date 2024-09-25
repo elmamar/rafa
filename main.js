37288a954409da3221e8d3e0a61906d6341b1f99
rafaela <drafaela926@gmail.com>
2024-09-25 11:46:07 -0300

    j

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ function soma(num1,num2){
 
 }
 console.log(soma (3,4));
-<<<<<<< HEAD
+
 function multiplicacion(num2,num3){
    return num2*num3;
-=======
-function multiplicacion(num2,num3){
 
->>>>>>> 604c442d616a0f274b69a07c53f73a8f9dc55797
+function areaquadrado(lado){
+return lado**2;
+
 }
+console.log (areaquadrado(4));
